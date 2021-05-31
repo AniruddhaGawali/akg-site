5d180e7b748903f2057c43da64e4d0b3a1b7b054
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-05-31 08:57:24 +0000

    update navbar

--- src/MyComponets/Navbar/navbar.js
+++ src/MyComponets/Navbar/navbar.js
@@ -1,8 +1,23 @@
+import "./navbar.css";
+
 function Navbar() {
   return (
-  <>
-  this is navbar
-  </>);
+    <>
+      <nav>
+        <span className="nav_item"> Home</span>
+        <span className="nav_item"> Timeline</span>
+        <span>
+          <img
+            className="logo"
+            src="https://i.ibb.co/F6kqwHH/Pics-Art-05-31-01-28-58.jpg"
+            alt="Pics-Art-05-31-01-28-58"
+          />
+        </span>
+        <span className="nav_item"> Projects</span>
+        <span className="nav_item"> Learn</span>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
