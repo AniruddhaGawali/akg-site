0a9f1165806298bf153ae6423a656aabf23195f0
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-05-31 08:35:40 +0000

    initialise navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./MyComponets/Navbar/navbar"
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar/>
+    </>
+  );
 }
 
 export default App;
