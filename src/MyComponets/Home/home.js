ed57fa19dea44ebee805debaf1a471e649d7f0d1
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-06-01 09:23:42 +0000

    update home

--- src/MyComponets/Home/home.js
+++ src/MyComponets/Home/home.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "./home.css";
 
-function Home(){
-return (
-<>this is home</>
-);
+function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="info">
+          <h4>Hello!</h4>
+          <h1>Aniruddha Gawali</h1>
+          <h3>Coder | Student</h3>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Home;
