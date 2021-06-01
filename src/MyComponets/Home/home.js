4db19afffa560f62d19304fa801a85f985a4f95e
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-06-01 15:13:06 +0000

    update home

--- src/MyComponets/Home/home.js
+++ src/MyComponets/Home/home.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./home.css";
-import shape from '../../../public/shapes.svg'
+import shapes from "../../../public/shapes.svg";
 
 function Home() {
   return (
     <>
       <div className="container">
+        <img src={shapes} alt="" className="shapes" />
         <div className="info">
           <h4>Hello!</h4>
           <h1>Aniruddha Gawali</h1>
           <h3>Coder | Student</h3>
         </div>
-        <img src={shape}/>
       </div>
     </>
   );
