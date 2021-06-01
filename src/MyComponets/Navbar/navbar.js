c00645237b2592728b27f6c4fcfd1680ae158724
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-06-01 05:19:44 +0000

    nav upadte

--- src/MyComponets/Navbar/navbar.js
+++ src/MyComponets/Navbar/navbar.js
@@ -6,7 +6,6 @@ function Navbar() {
   const [imgurl, setImgUrl] = useState(
     "https://i.ibb.co/F6kqwHH/Pics-Art-05-31-01-28-58.jpg"
   );
-    const menu ()
   return (
     <>
       <nav>
@@ -21,23 +20,19 @@ function Navbar() {
           </Link>
         </span>
         <span className="logo_span">
-          <Link to="/" className="links">
-            <img
-              className="logo"
-              src={imgurl}
-              alt="AKG"
-              onMouseOver={() => {
-                setImgUrl(
-                  "https://i.ibb.co/dWMYT20/Pics-Art-06-01-08-32-27.png"
-                );
-              }}
-              onMouseOut={() => {
-                setImgUrl(
-                  "https://i.ibb.co/F6kqwHH/Pics-Art-05-31-01-28-58.jpg"
-                );
-              }}
-            />
-          </Link>
+          {/* <Link to="/" className="links"> */}
+          <img
+            className="logo"
+            src={imgurl}
+            alt="AKG"
+            onMouseOver={() => {
+              setImgUrl("https://i.ibb.co/dWMYT20/Pics-Art-06-01-08-32-27.png");
+            }}
+            onMouseOut={() => {
+              setImgUrl("https://i.ibb.co/F6kqwHH/Pics-Art-05-31-01-28-58.jpg");
+            }}
+          />
+          {/* </Link> */}
         </span>
         <span className="nav_item">
           <Link to="/" className="links">
