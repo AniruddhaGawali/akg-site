0159a528c52562a23c44cb7502f7b25b04d8195f
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-06-02 13:32:06 +0530

    Update home.js

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 import "./home.css";
 import shapes from "../../public/shapes.svg";
 import shapes_c from "../../public/shape-c.svg";
