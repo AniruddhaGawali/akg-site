85b710b5b8ea2ccf517d0786d5821ff06c0fe011
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-06-03 16:54:30 +0000

    Add files via upload

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,53 +1,41 @@
-
-import React, { useState } from "react";
-import { useSpring, animated } from "react-spring";
-import "./home.css";
-import shapes from "../../public/shapes.svg";
-import shapes_c from "../../public/shape-c.svg";
+import React from 'react';
+import './home.css';
+import Shape from './shape.svg';
+import Circle from './circle.svg';
+import Typewriter from 'typewriter-effect';
 
 function Home() {
-  const [Hand, setHand] = useState(false);
-
-  const wave_shake = useSpring({
-    rotate: Hand ? "0deg":"30deg"
-  });
-
   return (
     <>
-      <div className="container">
-        <img src={shapes} alt="" className="shapes" />
-        <img src={shapes_c} alt="" className="shapes_c" />
-        <div className="info">
-          <h4>Hello!</h4>
-          <h1>Aniruddha Gawali</h1>
-          <h3>Coder | Student</h3>
-        </div>
-        <div className="bimoji">
-          {/* <img
-            src="https://i.ibb.co/QkWYsXM/Pics-Art-05-30-05-41-22.png"
-            alt=""
-          /> */}
-          <img
-            src="https://i.ibb.co/VMCyHW6/Pics-Art-06-02-11-25-54.png"
-            alt=""
-            id="body"
-          />
-          <animated.div id="hand" className={wave_shake}>
-            <img
-              src="https://i.ibb.co/tMdRkxB/Pics-Art-06-02-11-21-22.png"
-              alt=""
-            />
-            <div id="fingur">
-              <img
-                src="https://i.ibb.co/MZcJMbT/Pics-Art-06-02-11-22-43.png"
-                alt=""
+      <div className="main">
+        <div className="container">
+          <div className="info">
+            <div className="greet">
+              <Typewriter
+                onInit={typewriter => {
+                  typewriter
+                    .pauseFor(3000)
+                    .typeString('Hello!')
+                    .start();
+                }}
               />
-              </div>
-            </animated.div>
-           <button onClick={()=>{setHand(!Hand)}}> click it </button>
+            </div>
+            <h1>Aniruddha Gawali</h1>
+            <h3>Coder | Student</h3>
+          </div>
+          <div className="bimoji">
+            <img
+              src="https://i.ibb.co/QkWYsXM/Pics-Art-05-30-05-41-22.png"
+              alt="bimoji"
+              className="bimoji"
+            />
+          </div>
         </div>
       </div>
+      <img src={Shape} alt="" className="shape" />
+      <img src={Circle} alt="" className="circle_shape" />
     </>
   );
 }
+
 export default Home;
