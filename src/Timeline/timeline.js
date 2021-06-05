4756a83f41655c04c9c86ea7ed9e2a39379758cd
AniruddhaGawali <aniruddhagawali05@gmail.com>
2021-06-05 12:29:53 +0000

    update timeline

--- src/Timeline/timeline.js
+++ src/Timeline/timeline.js
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState } from 'react';
+import Dates from './data';
+import './timeline.css';
 
-function Timeline(){
-return (
-<>this is timeline</>
-);
+function Timeline() {
+  const [title, setTitle] = useState(Dates[0].title);
+  const [dates, setDates] = useState(Dates[0].dates);
+  const [context, setContext] = useState(Dates[0].context);
+  const [old_e, setold_e] = useState(null);
+  const [style_, setStyle_] = useState('active');
+
+  function change_content(index) {
+    setTitle(Dates[index].title);
+    setDates(Dates[index].dates);
+    setContext(Dates[index].context);
+  }
+  function changecolor(e) {
+    if (old_e === null) {
+      e.target.style.background = 'var(--main-color)';
+      e.target.style.color = 'var(--text-color)';
+      setStyle_('');
+      setold_e(e);
+    } else {
+      old_e.target.style.background = 'transparent';
+      old_e.target.style.color = 'var(--text-color2)';
+      e.target.style.background = 'var(--main-color)';
+      e.target.style.color = 'var(--text-color)';
+      setold_e(e);
+    }
+  }
+
+  return (
+    <>
+      <div className="timeline">
+        <div className="date">
+          {Dates.map((dates, index) => {
+            if (index === 0) {
+              return (
+                <h1
+                  onClick={e => {
+                    changecolor(e);
+                    change_content(index);
+                  }}
+                  id={style_}
+                >
+                  {dates.date}
+                </h1>
+              );
+            } else {
+              return (
+                <h1
+                  onClick={e => {
+                    changecolor(e);
+                    change_content(index);
+                  }}
+                >
+                  {dates.date}
+                </h1>
+              );
+            }
+          })}
+        </div>
+        <div className="content">
+          <div>
+            <h2 className="title">{title}</h2>
+            <h3 className="dates">{dates}</h3>
+            <p className="context">{context}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Timeline;
