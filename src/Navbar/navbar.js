113efee393880317bd5572ad7b11c37c837bff93
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2021-06-05 14:00:19 +0000

    Update navbar.js

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -5,115 +5,65 @@ import { useSpring, animated } from 'react-spring';
 
 function Navbar({ img_url }) {
   const [imgurl, setImgUrl] = useState(img_url);
-  console.log(img_url);
-  const [id1, setid1] = useState('active');
-  const [id2, setid2] = useState('');
-  const [id3, setid3] = useState('');
-  const [id4, setid4] = useState('');
-
-  const [oldidno, setoldid] = useState(1);
+  const [oldidno, setoldid] = useState(null);
+  const [first_class, setclass] = useState('links active');
 
   const [menu, setMenu] = useState(false);
-
   const display_set = useSpring({
     display: menu ? 'none' : 'block'
   });
 
-  function change_nav_item(id_no) {
-    if (oldidno === 1) {
-      setid1('');
-    } else if (oldidno === 2) {
-      setid2('');
-    } else if (oldidno === 3) {
-      setid3('');
-    } else if (oldidno === 4) {
-      setid4('');
+  function change_nav_item(e) {
+    if (oldidno !== null) {
+      if (e.target.classList == 'nav_item' || e.target.classList == 'links') {
+        e.target.classList.add('active');
+        setoldid(e);
+        oldidno.target.classList.remove('active');
+      }
+    } else {
+      if (e.target.classList == 'nav_item' || e.target.classList == 'links') {
+        e.target.classList.add('active');
+        setoldid(e);
+        setclass('links');
+      }
     }
-
-    if (id_no === 1) {
-      setid1('active');
-    } else if (id_no === 2) {
-      setid2('active');
-    } else if (id_no === 3) {
-      setid3('active');
-    } else if (id_no === 4) {
-      setid4('active');
-    }
-
-    setoldid(id_no);
   }
-
   return (
     <>
       <nav>
-        <animated.span className="nav_item" style={display_set}>
-          <Link
-            to="/"
-            className="links"
-            id={id1}
-            onClick={() => {
-              change_nav_item(1);
-            }}
-          >
+        <animated.span
+          className="nav_item"
+          style={display_set}
+          onClick={change_nav_item}
+        >
+          <Link to="/" className={first_class}>
             <span class="material-icons">home</span>
             Home
           </Link>
         </animated.span>
-        <animated.span className="nav_item" style={display_set}>
-          <Link
-            to="/timeline"
-            className="links"
-            id={id2}
-            onClick={() => {
-              change_nav_item(2);
-            }}
-          >
+        {/* <animated.span className="nav_item" style={display_set}>
+          <Link to="/timeline" className="links" onClick={change_nav_item}>
             <span class="material-icons">event</span>
             Timeline
           </Link>
-        </animated.span>
+        </animated.span> */}
         <span className="logo_span">
-          {/* <Link to="/" className="links"> */}
           <img
             className="logo"
             src={imgurl}
-            alt="AKG"
-            onMouseOver={() => {
-              setImgUrl('https://i.ibb.co/dWMYT20/Pics-Art-06-01-08-32-27.png');
-            }}
-            onMouseOut={() => {
-              setImgUrl(img_url);
-            }}
-            onClick={() => {
-              if (window.matchMedia('(max-width:660px)').matches) {
-                setMenu(!menu);
+@@ -64,15 +90,30 @@ function Navbar({ img_url }) {
               }
             }}
           />
-          {/* </Link> */}
         </span>
         <animated.span className="nav_item" style={display_set}>
-          <Link
-            to="/"
-            className="links"
-            id={id3}
-            onClick={() => {
-              change_nav_item(3);
-            }}
-          >
+          <Link to="/" className="links" onClick={change_nav_item}>
             <span class="material-icons">code</span>
             Projects
           </Link>
         </animated.span>
         <animated.span className="nav_item" style={display_set}>
-          <Link
-            to="/"
-            className="links"
-            id={id4}
-            onClick={() => {
-              change_nav_item(4);
-            }}
-          >
+          <Link to="/" className="links" onClick={change_nav_item}>
             <span class="material-icons">school</span>
             Learn
           </Link>
@@ -122,4 +72,5 @@ function Navbar({ img_url }) {
     </>
   );
 }
+
 export default Navbar;
